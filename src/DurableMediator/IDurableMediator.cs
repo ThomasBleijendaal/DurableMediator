4af6a997e122a25fe74b4a303af0460cc501b966
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2022-12-20 20:57:24 +0100

    Docs

--- src/DurableMediator/IDurableMediator.cs
+++ src/DurableMediator/IDurableMediator.cs
@@ -2,7 +2,24 @@
 
 public interface IDurableMediator
 {
+    /// <summary>
+    /// Do not use this interface
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     Task SendObjectAsync(MediatorRequest request);
+
+    /// <summary>
+    /// Do not use this interface
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     Task<MediatorResponse> SendObjectWithResponseAsync(MediatorRequestWithResponse request);
+
+    /// <summary>
+    /// Do not use this interface
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
     Task<MediatorResponse> SendObjectWithCheckAndResponseAsync(MediatorRequestWithCheckAndResponse request);
 }
