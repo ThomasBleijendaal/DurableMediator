b591608ba3404ddcb6bb10f3b836306e2828cfc4
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2023-02-12 17:42:57 +0100

    oop functions added

---
+++ example/OutOfProcessFunctionApp/Function1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+
+namespace OutOfProcessFunctionApp;
+
+public class Function1
+{
+    private readonly ILogger _logger;
+
+    public Function1(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Function1>();
+    }
+
+    [Function(nameof(StartHelloCitiesUntypedAsync))]
+    public async Task<HttpResponseData> StartHelloCitiesUntypedAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        [DurableClient] DurableTaskClient client,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger(nameof(StartHelloCitiesUntypedAsync));
+
+        var instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(HelloCitiesUntypedAsync));
+        logger.LogInformation("Created new orchestration with instance ID = {instanceId}", instanceId);
+
+        return client.CreateCheckStatusResponse(req, instanceId);
+    }
+
+    [Function(nameof(HelloCitiesUntypedAsync))]
+    public static async Task<string> HelloCitiesUntypedAsync([OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var result = "";
+        result += await context.CallActivityAsync<string>(nameof(SayHelloUntyped), "Tokyo") + " ";
+        result += await context.CallActivityAsync<string>(nameof(SayHelloUntyped), "London") + " ";
+        result += await context.CallActivityAsync<string>(nameof(SayHelloUntyped), "Seattle");
+        return result;
+    }
+
+    [Function(nameof(SayHelloUntyped))]
+    public static string SayHelloUntyped([ActivityTrigger] string cityName, FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger(nameof(SayHelloUntyped));
+        logger.LogInformation("Saying hello to {name}", cityName);
+        return $"Hello, {cityName}!";
+    }
+}
